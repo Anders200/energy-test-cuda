1c2d2367799c133584943d66e79f5a0f5803701f
pat8os <patsy.gorka@gmail.com>
2026-02-03 22:36:00 +0100

    added basic cuda kernel for 2 dim. cleaned up file structure

---
+++ include/common/energy_types.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+struct Points2D {
+    const double* x;
+    const double* y;
+    int n;
+};
---
+++ include/cuda/energy_cuda.hpp
@@ -0,0 +1,7 @@
+#pragma once
+#include "common/energy_types.hpp"
+
+double energy_statistic_cuda(
+    const Points2D& X,
+    const Points2D& Y
+);
